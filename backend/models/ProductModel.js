a61451ad881bba90d13b280ebfaab3106d81eab1
unknown <srimandgl2004@gmail.com>
2025-02-07 21:49:10 +0530

    Added timestamps to product and user schemas

--- backend/models/ProductModel.js
+++ backend/models/ProductModel.js
@@ -1,28 +1,52 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true, min: 0 },
-  rating: { type: Number, required: true, min: 0, max: 5 },
-  brand: { type: String, required: true },
-  description: { type: String, required: true },
-  platform: { type: String, required: true },
-  link: {
+  name: { 
     type: String,
-    required: true,
-    validate: {
-      validator: (v) => /^https?:\/\/[^\s$.?#].[^\s]*$/.test(v),
-      message: (props) => `${props.value} is not a valid URL`,
-    },
+    required: true
+  },
+  price: { 
+    type: Number, 
+    required: true, 
+    min: 0 
+  },
+  rating: { 
+    type: Number, 
+    required: true, 
+    min: 0, 
+    max: 5 
+  },
+  brand: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  platform: { 
+    type: String, 
+    required: true 
+  },
+  link: {
+     type: String,
+     required: true,
+     validate: {
+       validator: (v) => /^https?:\/\/[^\s$.?#].[^\s]*$/.test(v),
+       message: (props) => `${props.value} is not a valid URL`,
+     },
   },
   imageUrl: {
-    type: String,
-    required: true,
-    validate: {
+     type: String,
+     required: true,
+     validate: {
       validator: (v) => /^https?:\/\/[^\s$.?#].[^\s]*$/.test(v),
       message: (props) => `${props.value} is not a valid image URL`,
     },
   },
-});
+},
+{ timestamps: true }
+);
+
 
 module.exports = mongoose.model('Product', productSchema);
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+  },
+  { timestamps: true }
+  );
+  
 // Hash the password before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
