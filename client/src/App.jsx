8a28c8ea6ba4b15ec7fe374954ed14d99b2b8224
unknown <srimandgl2004@gmail.com>
2025-02-10 10:45:51 +0530

    Initial React setup with Vite CLI and Tailwind.css

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Complete React SetUp </h1>
+    </div>
+  );
+}
+
+export default App;
