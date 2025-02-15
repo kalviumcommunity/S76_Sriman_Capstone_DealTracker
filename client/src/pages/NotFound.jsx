7a16139acd7e42051ae91b456b35e95668089edf
unknown <srimandgl2004@gmail.com>
2025-02-15 10:37:42 +0530

    Matching Desgin and add to readme file

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-white">
+      {/* 404 Error Message */}
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="text-gray-400 mt-2">Oops! The page you are looking for does not exist.</p>
+      
+      {/* Description */}
+      <p className="text-gray-400 mt-2">
+        Oops! The page you are looking for does not exist.
+      </p>
     </div>
   );
 };
